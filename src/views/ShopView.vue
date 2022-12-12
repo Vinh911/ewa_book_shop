57088a0fb4470a43e624fb615d4f4105840cb755
vnguyen <vnguyen@queo-group.com>
2022-12-12 22:37:29 +0100

    fetch and display data from database

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,10 +1,34 @@
+<script>
+    const API_URL = `https://ivm108.informatik.htw-dresden.de/ewa/g02/index.php`
+
+    export default {
+        data: () => ({
+            books: null
+        }),
+        created() {
+            // fetch on init
+            this.fetchData()
+        },
+        methods: {
+            async fetchData() {
+            this.books = await (await fetch(`${API_URL}`)).json()
+            }
+        }
+    }
+</script>
+
 <template>
     <div class="shop">
-      <h1>This is the shop page</h1>
-      
+        <h1>This is the shop page</h1>
+        <ul>
+            <li v-for="{ProduktID, Produkttitel} in books">
+                <p>{{ ProduktID }}</p>
+                <p>{{ Produkttitel }}</p>
+            </li>
+        </ul>
+
     </div>
-  </template>
+</template>
   
-  <style>
-  </style>
-  
+<style>
+</style>
