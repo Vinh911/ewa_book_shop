955ec551088c6f7845e6171056d374098ecf9cfa
vnguyen <vnguyen@queo-group.com>
2022-12-13 17:07:26 +0100

    reformat code

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,34 +1,35 @@
-<script>
-    const API_URL = `https://ivm108.informatik.htw-dresden.de/ewa/g02/index.php`
+<script setup>
 
-    export default {
-        data: () => ({
-            books: null
-        }),
-        created() {
-            // fetch on init
-            this.fetchData()
-        },
-        methods: {
-            async fetchData() {
-            this.books = await (await fetch(`${API_URL}`)).json()
-            }
-        }
+const API_URL = `https://ivm108.informatik.htw-dresden.de/ewa/g02/index.php`
+
+export default {
+  data: () => ({
+    books: null
+  }),
+  created() {
+    // fetch on init
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      this.books = await (await fetch(`${API_URL}`)).json()
     }
+  }
+}
 </script>
 
 <template>
-    <div class="shop">
-        <h1>This is the shop page</h1>
-        <ul>
-            <li v-for="{ProduktID, Produkttitel} in books">
-                <p>{{ ProduktID }}</p>
-                <p>{{ Produkttitel }}</p>
-            </li>
-        </ul>
+  <div class="shop">
+    <h1>This is the shop page</h1>
+    <ul>
+      <li v-for="{id, title} in books">
+        <p>{{ id }}</p>
+        <p>{{ title }}</p>
+      </li>
+    </ul>
 
-    </div>
+  </div>
 </template>
-  
+
 <style>
 </style>
